e7a271b235f1c21157c0f627d9e575d2fd36f7a8
gesnon <37512058+gesnon@users.noreply.github.com>
2020-02-10 10:13:38 +0400

    Нужно проверить

--- Logic/test/Test.cs
+++ Logic/test/Test.cs
@@ -1,24 +1,40 @@
 using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
 
-
-
 namespace Logic
-
 {
-
-    public class Test
-
+    public class CurrentRound  // экземпляр этого класс нужно будет реализовать в Game.cs
     {
-
-
+        public List<Card> RoundCards {get; set;} = new List(Card);
+        
+        public CurrentRound ()
+        {        
+        }
+        
+        public void AddCardOnBoard (Card card)
+        {
+            RoundCards.Add(card)
+        }
     }
-
+    
+    
+    
+    \\ этот метод надо будет добавить в класс Bot, он будет удалять карту из руки
+    public void DeleteCard (Card card)
+    {
+        for(int i =0; i<Hand.Count, i++)
+        {
+            if (Hand[i]==card)
+            {
+                Hand.RemoveAT(i);
+                break;
+            }
+            continue;
+        }
+    }
+    
+    
 }
